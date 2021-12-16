3111692fa47c7aadf242f07d88b3baaa2effffb0
AdrianAmerico <1fnadrian1@gmail.com>
2021-12-16 19:38:34 -0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import { Provider } from "react-redux";
+import SideBar from "./components/SideBar";
+import Video from "./components/VIdeo";
+import store from "./store";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Provider store={store}>
+          <Video />
+          <SideBar />
+        </Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
